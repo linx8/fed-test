a00747f0cc879c941c64061b97f5b7d2f2c8cc49
Linx <lincoln.le7@gmail.com>
2018-04-18 11:59:56 +1000

    project setup and tools chosen, ready to dev

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './scss/index.scss';
+
+ReactDOM.render(
+  <div>Carter Digital Fed Test - Lincoln Le</div>,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const config = {
+  entry: [
+    './src/index.js'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          }, {
+            loader: "css-loader"
+          }, {
+            loader: "sass-loader"
+          }]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
+
+module.exports = config;
